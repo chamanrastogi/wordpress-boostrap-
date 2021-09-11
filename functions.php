e7ec3957472b23ccb13d4263cb5b08c419f206c9
chamanrastogi <geniousarts2009@gmail.com>
2021-09-11 12:12:14 +0530

    Boostrap Blog theme

---
+++ functions.php
@@ -0,0 +1,236 @@
+<?php
+// Theme Support
+function bootstrapthme_setup() {
+	/* Sets up the content width value based on the theme's design. */
+	add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
+	add_theme_support( 'custom-logo', array(
+		'height'      => 100,
+		'width'       => 400,
+		'flex-height' => true,
+		'flex-width'  => true,
+		'header-text' => array( 'site-title', 'site-description' ),
+	) );
+	add_theme_support( 'title-tag' );
+	/* This theme styles the visual editor with editor-style.css to match the theme style. */
+	add_editor_style( 'css/editor_style.css' );
+	/* Adds RSS feed links to <head> for posts and comments. */
+	add_theme_support( 'automatic-feed-links' );
+	/* Switches default core markup for search form, comment form, and comments to output valid HTML5. */
+	add_theme_support( 'html5', array( 'comment-form', 'comment-list' ) );
+	/* This theme supports all available post formats by default. See http://codex.wordpress.org/Post_Formats */
+	add_theme_support( 'post-formats',
+		array(
+			'aside',
+			'audio',
+			'chat',
+			'gallery',
+			'image',
+			'link',
+			'quote',
+			'status',
+			'video',
+		)
+	);
+	add_theme_support( 'custom-header',
+		array(
+			'default-image'          => '',
+			'width'                  => 1920,
+			'height'                 => 245,
+			'flex-width'             => false,
+			'flex-height'            => false,
+			'random-default'         => false,
+			'header-text'            => true,
+			'default-text-color'     => '#444',
+			'uploads'                => true,
+			'wp-head-callback'       => 'bootstrapthme_header_style',
+			'admin-head-callback'    => '',
+			'admin-preview-callback' => '',
+		)
+	);
+	/* This theme supports custom background color and image, and here we also set up the default background color. */
+	add_theme_support( 'custom-background',
+		array(
+			'default-color' => 'fff',
+		)
+	);
+	/* This theme uses a custom image size for featured images, displayed on "standard" posts. */
+	add_theme_support( 'post-thumbnails' );
+	add_image_size( 'best_image_content_size', 656, 9999 ); /* Unlimited height, soft crop */
+
+	
+	register_nav_menus( array(
+		'menu-1' => esc_html__( 'Header', 'bootstrapthme' ),'primary' => esc_html__( 'Primary Menu', 'bootstrapthme' ),'footer' => esc_html__( 'Footer Menu', 'bootstrapthme' )
+	) );
+}
+
+if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
+    // File does not exist... return an error.
+    return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+} else {
+    // File exists... require it.
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+	function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+
+
+function bootstrapthme_header_style() {
+	$text_color   = get_header_textcolor();
+	$display_text = display_header_text();
+	/* If no custom options for text are set, return default. */
+	if ( HEADER_TEXTCOLOR == $text_color ) {
+		return;
+	}
+	/* If optins are set, we use them  */ ?>
+	<style type="text/css">
+		<?php /*If the user has set a custom color for the text use that */
+		if ( 'blank' != $text_color ) { ?>
+			.best-site-title,
+			.best-site-title a {
+				color: <?php echo '#' . $text_color . '!important'; ?>;
+			}
+		<?php }
+		/* Display text or not */
+		if ( ! $display_text ) { ?>
+			.best-site-title {
+				display: none;
+			}
+		<?php } ?>
+	</style>
+<?php }
+
+
+
+ //Add HTML5 theme support.
+ 
+function wpdocs_after_setup_theme() {
+    add_theme_support( 'html5', array( 'search-form' ) );
+}
+add_action( 'after_setup_theme', 'wpdocs_after_setup_theme' );
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function bootstrapthme_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Home Banner Slider',
+		'id'            => 'home_banner',
+		'before_widget' => '',
+		'after_widget'  => '',
+		'before_title'  => '',
+		'after_title'   => '',
+	) );
+	register_sidebar( array(
+		'name'          => 'Home Sidebar',
+		'id'            => 'home_sidebar',
+		'before_widget' => '<aside class="home_sidewidget">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>',
+	) );
+	register_sidebar( array(
+		'name'          => 'Home Footer Widget',
+		'id'            => 'home_footer',
+		'before_widget' => '<aside class="home_sidewidget">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>',
+	) );
+	
+}
+add_action( 'widgets_init', 'bootstrapthme_widgets_init' );
+
+
+function slider_init() {
+    $args = array(
+      'label' => 'Image Slider',
+        'public' => true,
+        'show_ui' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'rewrite' => array('slug' => 'img_slider'),
+        'query_var' => true,
+        'menu_icon' => 'dashicons-admin-media',
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'custom-fields',
+            'thumbnail',
+            'author',
+            'page-attributes',)
+        );
+    register_post_type( 'img_slider', $args );
+}
+add_action( 'init', 'slider_init' );
+
+function client_init() {
+    $args = array(
+      'label' => 'Testimonials',
+        'public' => true,
+        'show_ui' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'rewrite' => array('slug' => 'client_say'),
+        'query_var' => true,
+        'menu_icon' => 'dashicons-format-chat',
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'custom-fields',
+            'thumbnail',
+            'author',
+            'page-attributes',)
+        );
+    register_post_type( 'client_say', $args );
+}
+add_action( 'init', 'client_init' );
+
+function team_init() {
+    $args = array(
+      'label' => 'Team Member',
+        'public' => true,
+        'show_ui' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'rewrite' => array('slug' => 'team_member'),
+        'query_var' => true,
+        'menu_icon' => 'dashicons-admin-users',
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'custom-fields',
+            'thumbnail',
+            'author',
+            'page-attributes',)
+        );
+    register_post_type( 'team_member', $args );
+}
+add_action( 'init', 'team_init' );
+/**
+ * Enqueue scripts and styles.
+ */
+function theme_files(){
+
+	    // Theme block stylesheet.
+		wp_enqueue_style('Main Style', get_template_directory_uri().'/style.css', array(), $theme_version, 'all');
+		wp_enqueue_style( 'Bootstrap 5','https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+	    // Theme block Javascript.	
+	    wp_enqueue_script('Bundle', get_template_directory_uri().'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',0,0,true);
+	 
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
+}
+
+
+add_action( 'after_setup_theme', 'bootstrapthme_setup' );
+
+add_action( 'wp_enqueue_scripts', 'theme_files' );
+
+?>
---
+++ header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html  <?php language_attributes(); ?>>
+
+
+<head>	
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+      
+	<!-- Place favicon.ico in the root directory -->
+	<link rel="shortcut icon" type="image/x-icon" href="assets/img/logo/favicon.png">
+	<!-- ========== Start Stylesheet ========== -->
+	<?php wp_head(); ?>
+	<!-- ========== End Stylesheet ========== -->
+  
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	  <script src="assets/js/html5/html5shiv.min.js"></script>
+	  <script src="assets/js/html5/respond.min.js"></script>
+	<![endif]-->
+	
+</head>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
